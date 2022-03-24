6b38978a92555087ed3aed000b8a7e68efd70bed
jia1995 <15927447546@163.com>
2022-03-24 21:23:14 +0800

    base version of bottom side bar.

--- Imitation vscode editor/bottomSidebar.py
+++ Imitation vscode editor/bottomSidebar.py
@@ -3,3 +3,8 @@
 @date    2022/3/24
 @context The editor's bottom sidebar.
 '''
+import tkinter as tk
+
+class BottomSideBar(tk.Frame):
+    def __init__(self, master, height=20, bg='blue') -> None:
+        tk.Frame.__init__(self, master, height=height, bg=bg)
