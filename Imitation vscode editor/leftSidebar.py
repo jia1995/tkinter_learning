9f2312cdbb6c388a59ead013ad049742153a6319
jia1995 <15927447546@163.com>
2022-03-24 21:27:44 +0800

    base version of left side bar.

--- Imitation vscode editor/leftSidebar.py
+++ Imitation vscode editor/leftSidebar.py
@@ -3,3 +3,8 @@
 @date    2022/3/24
 @context The editor's left sidebar.
 '''
+import tkinter as tk
+
+class LeftSideBar(tk.Frame):
+    def __init__(self, master, width=40, bg='gray'):
+        tk.Frame.__init__(self, master, width=width, bg=bg)
